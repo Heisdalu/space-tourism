bdedf908516ca0b47d92fb13073b6d1b3f9f154f
divine <divineobi07@gmail.com>
2022-08-08 12:50:00 +0100

    add destination component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Home from "./components/Home/Home";
+import Destination from "./components/Destination/Destination";
 
 const App = () => {
-  return <div className="main-wrapper">
-   <Navigation />
-   <div className='subWrapper'>
-   <Home />
-
-   </div>
-  </div>
-}
+  return (
+    <div className="main-wrapper">
+      <Navigation />
+      <div className="subWrapper">
+        {/* <Home /> */}
+        <Destination />
+        </div>
+    </div>
+  );
+};
 
 export default App;
