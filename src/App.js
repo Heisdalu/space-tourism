477b93d5c84f636199d9ede3f502a6542f661bee
divine <divineobi07@gmail.com>
2022-08-15 11:17:39 +0100

    edit home component css styles

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from "react";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Home from "./components/Home/Home";
-import Destination from "./components/Destination/Destination";
-import Crew from "./components/Crew/Crew";
-import Technology from "./components/Technology/Technology";
+// import Destination from "./components/Destination/Destination";
+// import Crew from "./components/Crew/Crew";
+// import Technology from "./components/Technology/Technology";
 
 const App = () => {
   return ( 
     <div className="main-wrapper">
       <Navigation />
       <div className="subWrapper">
-        {/* <Home /> */}
+        <Home />
         {/* <Destination /> */}
         {/* <Crew /> */}
-        <Technology />
+        {/* <Technology /> */}
         </div>
     </div>
   );
