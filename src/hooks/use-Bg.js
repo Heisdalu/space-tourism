de20fb186fc00afe2016a2314e0c7984e15aa04e
divine <divineobi07@gmail.com>
2022-08-20 18:21:33 +0100

    add hooks and lib folder

---
+++ src/hooks/use-Bg.js
@@ -0,0 +1,15 @@
+import { useEffect, useContext } from "react";
+import { CHANGE_BG } from "../lib/bg";
+import AppContext from "../store/app-context";
+
+// change background pic of different component
+const useBg = (value) => {
+  const { changeBg } = useContext(AppContext);
+
+  console.log(value);
+  useEffect(() => {
+    changeBg(CHANGE_BG, value);
+  }, [changeBg, value]);
+};
+
+export default useBg;
---
+++ src/lib/bg.js
@@ -0,0 +1,5 @@
+export const CHANGE_BG = "bg-change";
+export const HOME_BG = "homeBg";
+export const DESTINATION_BG = "destBg";
+export const CREW_BG = "crewbg";
+export const TECH_BG = "techBg";
